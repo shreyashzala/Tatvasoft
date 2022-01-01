f2764391cbc81750d4773c72ee24e71eec0dd34f
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-01-01 12:27:01 +0530

    Delete js.js

--- HTML/js.js
+++
@@ -1,7 +0,0 @@
-
-    window.addEventListener('scroll', function () {
-      let nav = document.querySelector('nav');
-      let windowPosition = window.scrollY > 0;
-      nav.classList.toggle('scrolling-active', windowPosition);
-
-    })
