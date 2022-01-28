9342692f0ff30d84d9e667a3993e584f63c0566a
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-01-28 18:53:47 +0530

    Add files via upload

---
+++ PHP/assests/js/home.js
@@ -0,0 +1,7 @@
+
+    window.addEventListener('scroll', function () {
+      let nav = document.querySelector('nav');
+      let windowPosition = window.scrollY > 0;
+      nav.classList.toggle('scrolling-active', windowPosition);
+
+    })
