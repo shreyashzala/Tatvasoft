5fe75b1b77a7194666f9f5ae22f62cb56e341b5e
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-01-28 19:07:42 +0530

    Create about.php

---
+++ PHP/Views/about.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+
+    <link rel="stylesheet" href="./assets/css/about.css">
+
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="./assets/css/all.min.css">
+</head>
+<body>
+    <!-- navbar -->
+<?php 
+     include('navbar.php');
+?>
+
+        <div class="banner">
+            <img src="./assets/image/hero-banner-img.png" alt="About">
+        </div>
+        
+        <div class="title">
+            <h1>A Few words about us</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <hr class="line-1">
+            </div>
+            <div class="col-md-4">
+                <img src="./assets/image/forma-1-copy-5.png" alt="logo" class="logo">
+            </div>
+            <div class="col-md-4">
+                <hr class="line-2">
+            </div>
+        </div>
+        <div class="content">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie convallis tempor. Duis vestibulum vel risus condimentum dictum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus quis enim orci. Fusce risus lacus, sollicitudin eu magna sed, pharetra sodales libero. Proin tincidunt vel erat id porttitor. Donec tristique est arcu, sed dignissim velit vulputate ultricies. <br><br>
+            Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Vivamus eget mauris eget nisl euismod volutpat sed sed libero. Quisque sit amet lectus ex. Ut semper ligula et mauris tincidunt hendrerit. Aenean ut rhoncus orci, vel elementum turpis. Donec tempor aliquet magna eu fringilla. Nam lobortis libero ut odio finibus lobortis. In hac habitasse platea dictumst. Mauris a hendrerit felis. Praesent ac vehicula ipsum, at porta tellus. Sed dolor augue, posuere sed neque eget, aliquam ultricies velit. Sed lacus elit, tincidunt et massa ac, vehicula placerat lorem.</p>
+        </div>
+        <div class="title">
+            <h1>Our Story</h1>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <hr class="line-1">
+            </div>
+            <div class="col-md-4">
+                <img src="./assets/image/forma-1-copy-5.png" alt="logo" class="logo">
+            </div>
+            <div class="col-md-4">
+                <hr class="line-2">
+            </div>
+        </div>
+
+        <div class="text">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie convallis tempor. Duis vestibulum vel risus condimentum dictum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus quis enim orci. Fusce risus lacus, sollicitudin eu magna sed, pharetra sodales libero. Proin tincidunt vel erat id porttitor. Donec tristique est arcu, sed dignissim velit vulputate ultricies. <br><br>
+                Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Vivamus eget mauris eget nisl euismod volutpat sed sed libero. Quisque sit amet lectus ex. Ut semper ligula et mauris tincidunt hendrerit. <br><br>
+                Aenean ut rhoncus orci, vel elementum turpis. Donec tempor aliquet magna eu fringilla. Nam lobortis libero ut odio finibus lobortis. In hac habitasse platea dictumst. Mauris a hendrerit felis. Praesent ac vehicula ipsum, at porta tellus. Sed dolor augue, posuere sed neque eget, aliquam ultricies velit. Sed lacus elit, tincidunt et massa ac, vehicula placerat lorem.
+            </p>
+        </div>
+
+      <!-- Footer -->
+<?php 
+      include('footer.php');
+?>
