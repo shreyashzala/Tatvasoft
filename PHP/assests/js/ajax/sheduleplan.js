2f2599461fdf6c992d2bc7e62cc694068f94d455
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-02-19 15:04:11 +0530

    Add files via upload

---
+++ PHP/assests/js/ajax/sheduleplan.js
@@ -0,0 +1,9 @@
+$(document).ready(function(e){
+    var amount = $("#amount").text();
+    var total_amount = amount*4;
+    $('#totalPayment').html('<span>'+total_amount+'</span>');
+    
+    
+    
+});
+            
