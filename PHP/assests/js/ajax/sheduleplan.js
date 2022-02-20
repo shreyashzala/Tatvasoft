556b6e17a4887d6832c4e1798ec1603ebc3ceb4d
shreyashzala <95066802+shreyashzala@users.noreply.github.com>
2022-02-20 16:06:30 +0530

    Delete sheduleplan.js

--- PHP/assests/js/ajax/sheduleplan.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(e){
-    var amount = $("#amount").text();
-    var total_amount = amount*4;
-    $('#totalPayment').html('<span>'+total_amount+'</span>');
-    
-    
-    
-});
-            
